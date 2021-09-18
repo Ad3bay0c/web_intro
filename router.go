915808069e746109c9b5af648b24a035f3a18443
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:48:13 +0100

    Home Handler Added

--- router.go
+++ router.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/Ad3bay0c/web_intro/models"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
 	r := chi.NewRouter()
 
-	//r.Get("/", )
+	r.Get("/", models.Home)
+
+	log.Printf("Server Started at Localhost:4000")
+	log.Fatalf("%v", http.ListenAndServe(":4000", r))
 }
