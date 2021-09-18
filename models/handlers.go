3fd1daa34bad17ac0ff123c3c530cddbd1aaa999
tambarie <aclintonadebayo@gmail.com>
2021-09-18 22:45:09 +0100

    Creating Blog Post Accepted

--- models/handlers.go
+++ models/handlers.go
@@ -4,14 +4,51 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
+var (
+	blogs = Blogs{}
+	message = Message{}
+)
+
+func init() {
+
+}
 func Home(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("frontend/home.gtpl")
+	t, err := template.ParseFiles("frontend/home.gtpl", "frontend/navbar.gtpl")
 	if err != nil {
 		log.Printf("Error Opening File: %v", err.Error())
 		return
 	}
+	err = t.Execute(w, message)
+	message.Message = ""
+	message.Color = ""
+	if err != nil {
+		log.Printf("Error Opening File: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
-	t.Execute(w, "Adebayo")
+func CreateBlog(w http.ResponseWriter, r *http.Request)  {
+	title := strings.TrimSpace(r.FormValue("title"))
+	details := strings.TrimSpace(r.FormValue("details"))
+
+	blog := Blog{
+		ID:      time.Now().Unix(),
+		Title:   title,
+		Details: details,
+		Date:    time.Now().Local(),
+	}
+	blogs.Blogs = append(blogs.Blogs, blog)
+	err := blogs.addToFile()
+	if err != nil {
+		log.Printf(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	message.Message = "Created Successfully"
+	message.Color 	= "success"
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
