3c74089ca5ad11b86686d157d0275c8c0755aa5e
tambarie <aclintonadebayo@gmail.com>
2021-09-18 23:44:31 +0100

    Delete Route Created

--- router.go
+++ router.go
@@ -11,6 +11,11 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", models.Home)
+	r.Post("/create", models.CreateBlog)
+
+	r.Route("/{id}", func(r chi.Router) {
+		r.Get("/delete", models.DeleteBlog)
+	})
 
 	log.Printf("Server Started at Localhost:4000")
 	log.Fatalf("%v", http.ListenAndServe(":4000", r))
