58d472beefa746f838cd341e343c1e4095e1c91b
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:53:03 +0100

    Home Handler Logic Added and home.gtpl

---
+++ frontend/home.gtpl
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Testing Web</title>
+</head>
+<body>
+    <h2>We Move and making progress {{ . }}</h2>
+</body>
+</html>
--- models/handlers.go
+++ models/handlers.go
@@ -1,7 +1,17 @@
 package models
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("frontend/home.gtpl")
+	if err != nil {
+		log.Printf("Error Opening File: %v", err.Error())
+		return
+	}
 
+	t.Execute(w, "Adebayo")
 }
