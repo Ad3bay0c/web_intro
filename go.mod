6925bb377c787061588ddf722cb2014902941c31
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:44:21 +0100

    Go chi Added and router instantiated

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ad3bay0c/web_intro
 
 go 1.16
+
+require github.com/go-chi/chi/v5 v5.0.4 // indirect
--- router.go
+++ router.go
@@ -1,1 +1,9 @@
-package web_intro
+package main
+
+import "github.com/go-chi/chi/v5"
+
+func main() {
+	r := chi.NewRouter()
+
+	//r.Get("/", )
+}
